0606b5d5ac0272d6dedbe2d50fd0cfe57f952fd8
JiageWang <1076050774@qq.com>
2023-07-20 14:36:15 +0800

    刻度文本调整

--- lib/timeline/core/timeline_controller.dart
+++ lib/timeline/core/timeline_controller.dart
@@ -14,9 +14,9 @@ class TimelineController {
   final double tickBackgroundWidth; // 左侧标签区域总宽度
   final Color tickColor; // 标签颜色
   final Color tickBackgroundColor; // 标签背景颜色
-  final Duration tickIntervalTime = const Duration(days: 1); // 标签时间间隔
-  final int tickSubCount = 3; // 标签时间间隔
   final double minScale = 60 / Duration.millisecondsPerDay;
+
+  // TODO 阈值
   final double thresholdScale = 900 / Duration.millisecondsPerDay;
   final double maxScale = 1500 / Duration.millisecondsPerDay;
 
@@ -39,7 +39,7 @@ class TimelineController {
 
   TimelineController(
       {required DateTime startTime,
-      this.tickWidth = 15,
+      this.tickWidth = 40,
       this.tickSubWidth = 5,
       this.tickIntervalHeight = 100,
       this.tickBackgroundWidth = 60,
@@ -50,8 +50,8 @@ class TimelineController {
   /// 初始化视图，根据height自动计算end
   void initViewport(double height) {
     _height = height;
-    _end = _start +
-        _height / (tickIntervalHeight / tickIntervalTime.inMilliseconds);
+    _end =
+        _start + _height / (tickIntervalHeight / Duration.millisecondsPerDay);
   }
 
   /// 更新视图
@@ -76,11 +76,7 @@ class TimelineController {
         Rect.fromLTWH(offset.dx, offset.dy, tickBackgroundWidth, size.height),
         backgroundPaint);
 
-    if(scale < thresholdScale){
-      paintTicksByDay(canvas, tickPaint, offset, size);
-    }else{
-      paintTicksByHour(canvas, tickPaint, offset, size);
-    }
+    paintTicksByDay(canvas, tickPaint, offset, size);
   }
 
   /// 动态绘制子组件
@@ -98,119 +94,61 @@ class TimelineController {
       final itemOffsetY = (childParentData.dateTime! - start) * scale;
       childParentData.offset = Offset(itemOffsetX, itemOffsetY);
 
-      if (childParentData.dateTime! + child.size.height / scale < start ||
-          childParentData.dateTime!  > end) {
-        child = childParentData.nextSibling;
-        continue;
+      // 子组件在画面内进行绘制
+      if (childParentData.dateTime! + child.size.height / scale > start &&
+          childParentData.dateTime! < end) {
+        context.paintChild(child, childParentData.offset + offset);
       }
-      context.paintChild(child,  childParentData.offset+ offset);
+
       child = childParentData.nextSibling;
     }
   }
 
-  void paintTicksByDay(Canvas canvas, Paint tickPaint, Offset offset, Size size) {
-
+  void paintTicksByDay(
+      Canvas canvas, Paint tickPaint, Offset offset, Size size) {
     // 计算tick所在时间节点
     var tickTime =
-    DateTime(startTime.year, startTime.month, startTime.day - 1); // 日
-    var tick = tickTime.millisecondsSinceEpoch;
-    // 计算tick相对于start所在高度
-    var tickPosition = (tick - start) * scale;
-    var tickSubInternalTime = const Duration(hours: 24 ~/ 4);
-    var tickSubInternalHeight = tickSubInternalTime.inMilliseconds * scale;
+        DateTime(startTime.year, startTime.month, startTime.day - 1); // 日
+    var tickPosition = (tickTime.millisecondsSinceEpoch - start) * scale;
+    var tickInternal = Duration.millisecondsPerDay * scale;
+    var tickInternalTime = const Duration(days: 1);
+    var tickTextStype = const TextStyle(fontSize: 14);
 
     // 绘制刻度
     while (tickPosition <= size.height) {
-      if (tickPosition < 0) {
-        tickTime = tickTime.add(tickSubInternalTime);
-        tickPosition += tickSubInternalHeight;
-      } else {
-        if (tickTime.hour == 0) {
-          // 主刻度
-          canvas.drawRect(
-              Rect.fromLTWH(offset.dx + tickBackgroundWidth - tickWidth,
-                  tickPosition + offset.dy, tickWidth, 1.5),
-              tickPaint);
-          // 文本
+      if (tickPosition >= 0) {
+        // 主刻度
+        canvas.drawRect(
+            Rect.fromLTWH(offset.dx + tickBackgroundWidth - tickWidth,
+                tickPosition + offset.dy, tickWidth, 1.5),
+            tickPaint);
+        // 文本
+        TextPainter(
+            text: TextSpan(
+                text: TimelineUtils.formatDay(tickTime), style: tickTextStype),
+            textDirection: TextDirection.ltr,
+            ellipsis: '.')
+          ..layout(maxWidth: tickBackgroundWidth)
+          ..paint(canvas, Offset(offset.dx + 20, tickPosition + offset.dy + 2));
+        if (tickTime.month == 1 && tickTime.day == 1) {
           TextPainter(
-              text: TextSpan(text: TimelineUtils.formatDay(tickTime)),
+              text: TextSpan(
+                  text: tickTime.year.toString(),
+                  style: tickTextStype.copyWith(fontWeight: FontWeight.bold)),
               textDirection: TextDirection.ltr,
               ellipsis: '.')
-            ..layout(maxWidth: tickBackgroundWidth - tickWidth)
-            ..paint(canvas, Offset(offset.dx, tickPosition + offset.dy));
-          if (tickTime.month == 1 && tickTime.day == 1) {
-            TextPainter(
-                text: TextSpan(
-                    text: tickTime.year.toString(),
-                    style: const TextStyle(fontWeight: FontWeight.bold)),
-                textDirection: TextDirection.ltr,
-                ellipsis: '.')
-              ..layout(maxWidth: tickBackgroundWidth - tickWidth)
-              ..paint(canvas, Offset(offset.dx, tickPosition + offset.dy - 15));
-          }
-        } else {
-          // 副刻度
-          canvas.drawRect(
-              Rect.fromLTWH(offset.dx + tickBackgroundWidth - tickSubWidth,
-                  tickPosition + offset.dy, tickSubWidth, 1),
-              tickPaint);
+            ..layout(maxWidth: tickBackgroundWidth)
+            ..paint(
+                canvas, Offset(offset.dx + 20, tickPosition + offset.dy - 20));
         }
-        tickTime = tickTime.add(tickSubInternalTime);
-        tickPosition += tickSubInternalHeight;
       }
+      tickTime = tickTime.add(tickInternalTime);
+      tickPosition += tickInternal;
     }
-  }
-
-  void paintTicksByHour(Canvas canvas, Paint tickPaint, Offset offset, Size size) {
-
-    // 计算tick所在时间节点
-    var tickTime =
-    DateTime(startTime.year, startTime.month, startTime.day, startTime.hour - 1); // 日
-    var tick = tickTime.millisecondsSinceEpoch;
-    // 计算tick相对于start所在高度
-    var tickPosition = (tick - start) * scale;
-    var tickSubInternalTime = const Duration(minutes: 60 ~/ 4);
-    var tickSubInternalHeight = tickSubInternalTime.inMilliseconds * scale;
-
-    // 绘制刻度
-    while (tickPosition <= size.height) {
-      if (tickPosition < 0) {
-        tickTime = tickTime.add(tickSubInternalTime);
-        tickPosition += tickSubInternalHeight;
-      } else {
-        if (tickTime.minute == 0) {
-          // 主刻度
-          canvas.drawRect(
-              Rect.fromLTWH(offset.dx + tickBackgroundWidth - tickWidth,
-                  tickPosition + offset.dy, tickWidth, 1.5),
-              tickPaint);
-          // 文本
-          TextPainter(
-              text: TextSpan(text: TimelineUtils.formatTime(tickTime)),
-              textDirection: TextDirection.ltr,
-              ellipsis: '.')
-            ..layout(maxWidth: tickBackgroundWidth - tickWidth)
-            ..paint(canvas, Offset(offset.dx, tickPosition + offset.dy));
-          if (tickTime.month == 1 && tickTime.day == 1) {
-            TextPainter(
-                text: TextSpan(
-                    text: TimelineUtils.formatDate(tickTime),
-                    style: const TextStyle(fontWeight: FontWeight.bold)),
-                textDirection: TextDirection.ltr,
-                ellipsis: '.')
-              ..layout(maxWidth: tickBackgroundWidth - tickWidth)
-              ..paint(canvas, Offset(offset.dx, tickPosition + offset.dy - 15));
-          }
-        } else {
-          // 副刻度
-          canvas.drawRect(
-              Rect.fromLTWH(offset.dx + tickBackgroundWidth - tickSubWidth,
-                  tickPosition + offset.dy, tickSubWidth, 1),
-              tickPaint);
-        }
-        tickTime = tickTime.add(tickSubInternalTime);
-        tickPosition += tickSubInternalHeight;
-      }
-    }
+    //         // 副刻度
+    //         canvas.drawRect(
+    //             Rect.fromLTWH(offset.dx + tickBackgroundWidth - tickSubWidth,
+    //                 tickPosition + offset.dy, tickSubWidth, 1),
+    //             tickPaint);
   }
 }
